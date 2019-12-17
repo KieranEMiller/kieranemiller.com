7d8bc33c1d67415ab71b40367e1e89a95615d7d2
Kieran E. Miller <kieranemiller@gmail.com>
2019-12-17 11:48:42 -0500

    add projects and contact routing

--- src/wwwroot/assets/js/react/dev/routing.jsx
+++ src/wwwroot/assets/js/react/dev/routing.jsx
@@ -4,7 +4,8 @@ import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import Header from          './header.jsx';
 import Index from           './paths/index.jsx';
-import About from           './paths/about.jsx';
+import Contact from         './paths/contact.jsx';
+import Projects from        './paths/projects.jsx';
 import { Routes } from      './constants.jsx';
 
 class Routing extends React.Component {
@@ -14,7 +15,8 @@ class Routing extends React.Component {
                 <Switch>
                     <Route exact path={Routes.DEFAULT} component={Index} />
                     <Route exact path={Routes.INDEX} component={Index} />
-                    <Route component={Index} />
+                    <Route exact path={Routes.PROJECTS} component={Projects} />
+                    <Route exact path={Routes.CONTACT} component={Contact} />
                 </Switch>
             </div>
         );
