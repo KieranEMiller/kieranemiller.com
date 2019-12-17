7b9140b1fca016bb29ea3ffba3f7d14238226c22
Kieran E. Miller <kieranemiller@gmail.com>
2019-12-17 11:48:14 -0500

    1) remove login, about paths 2) remove mini header component as it is not used 3) add contact and projects paths

---
+++ src/wwwroot/assets/js/react/dev/paths/projects.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+
+import BaseComponent from './base_component.jsx';
+import ContentContainer from '../content.jsx';
+
+class Projects extends BaseComponent {
+    render() {
+        return (
+            <ContentContainer>
+                <div className="content_row">
+                    <h2>Projects</h2>
+                    <ul>
+                        <li>Employment opportunities: <a href="mailto:jobs@kieranemiller.com">jobs@kieranemiller.com</a></li>
+                        <li>LinkedIn: <a href="https://linkedin.com/KieranEMiller">https://linkedin.com/KieranEMiller</a></li>
+                    </ul>
+                </div>
+            </ContentContainer>
+        );
+    }
+}
+
+export default Projects;
